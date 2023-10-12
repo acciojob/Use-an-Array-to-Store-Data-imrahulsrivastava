e51917e6cce8c28bff86ab58992f73e82187f089
Rahul Kumar Srivastava <124518238+imrahulsrivastava@users.noreply.github.com>
2023-10-12 12:50:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+// Write your code here
 
+const simpleArray = [];
+
+simpleArray.push("start");
+simpleArray.push(true);
+simpleArray.push(1);
+simpleArray.push(false);
+simpleArray.push("end");
+console.log(simpleArray);
+
+const complexArray = [];
+
+const updateArray = function (keyA, valueA, keyB, valueB) {
+  const myObject = {};
+  myObject[keyA] = valueA;
+  myObject[keyB] = valueB;
+  complexArray.push(myObject);
+};
+
+updateArray("one", 1, "two", 2);
+updateArray("three", 3, "four", 4);
+updateArray("a", "a", "b", "b");
+updateArray("c", "c", "d", "d");
+
+console.log(complexArray);
